98f5c4cfcb96db875c4e915fa3e7430c95b65efc
stevenewald <47366039+stevenewald@users.noreply.github.com>
2019-04-27 21:01:42 -0400

    Update SolarPlane.py

--- Desktop/SolarPlane.py
+++ Desktop/SolarPlane.py
@@ -1,10 +1,13 @@
 import time
+import sys
 
 import navio.ms5611
 import navio.util
+import navio.leds
 
 navio.util.check_apm()
 
+led = navio.leds.Led()
 baro = navio.ms5611.MS5611()
 baro.initialize()
 
@@ -31,8 +34,16 @@ def MetersToFeet(meters):
 def deltaAltitude():
 	return MetersToFeet(HypsometricFormula(baro.PRES, baro.TEMP)) - Offset
 
+def LEDAboveBelow(Altitude):
+	if Altitude < 0:
+		led.setColor('Red')
+	else:
+		led.setColor('Green')
+
 Offset = MetersToFeet(HypsometricFormula(baro.PRES, baro.TEMP))
 	
+print "LED is red if negative feet, green if positive"
+
 i=0
 while(i<60):
 	baro.refreshPressure()
@@ -44,10 +55,13 @@ while(i<60):
 	baro.readTemperature()
 
 	baro.calculatePressureAndTemperature()
-
+	print "Initial Altitude:"
+	print Offset
 	print "ALTITUDE: "
-	#print MetersToFeet(HypsometricFormula(baro.PRES, baro.TEMP))
-	print deltaAltitude()
+	print MetersToFeet(HypsometricFormula(baro.PRES, baro.TEMP))
+	#print deltaAltitude()
+
+	LEDAboveBelow(deltaAltitude)
 
 	time.sleep(1)
 	
