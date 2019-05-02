3795d08528b9305dda97ace51d81ab0fb5caf7ed
stevenewald <47366039+stevenewald@users.noreply.github.com>
2019-05-02 12:23:41 -0400

    Update PID.py

--- Desktop/PID.py
+++ Desktop/PID.py
@@ -152,23 +152,23 @@ def kalmanFilterX ( accAngle, gyroRate, DT):
 	
 	return KFangleX
 
-
-gyroXangle = 0.0
-gyroYangle = 0.0
-gyroZangle = 0.0
-CFangleX = 0.0
-CFangleY = 0.0
-CFangleXFiltered = 0.0
-CFangleYFiltered = 0.0
-kalmanX = 0.0
-kalmanY = 0.0
-oldXMagRawValue = 0
-oldYMagRawValue = 0
-oldZMagRawValue = 0
-oldXAccRawValue = 0
-oldYAccRawValue = 0
-oldZAccRawValue = 0
 def EulerAngles():
+    gyroXangle = 0.0
+    gyroYangle = 0.0
+    gyroZangle = 0.0
+    CFangleX = 0.0
+    CFangleY = 0.0
+    CFangleXFiltered = 0.0
+    CFangleYFiltered = 0.0
+    kalmanX = 0.0
+    kalmanY = 0.0
+    oldXMagRawValue = 0
+    oldYMagRawValue = 0
+    oldZMagRawValue = 0
+    oldXAccRawValue = 0
+    oldYAccRawValue = 0
+    oldZAccRawValue = 0
+
     a = datetime.datetime.now()
 
 
