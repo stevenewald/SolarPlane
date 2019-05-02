3351237a72e3ad6343a2b7e0dfae8047885b75ad
stevenewald <47366039+stevenewald@users.noreply.github.com>
2019-05-02 12:15:49 -0400

    Update PID.py

--- Desktop/PID.py
+++ Desktop/PID.py
@@ -1,95 +1,510 @@
 import time
+import sys
+import navio.ms5611
+import navio.util
+import argparse 
+import navio.mpu9250
+import math
+import datetime
+import os
 
+
+navio.util.check_apm()
+
+imu = navio.mpu9250.MPU9250()
+
+if imu.testConnection():
+    print("Connection established: True")
+else:
+    sys.exit("Connection established: False")
+
+imu.initialize()
+
+time.sleep(1)
+
+
+# imu.read_all()
+# imu.read_gyro()
+# imu.read_acc()
+# imu.read_temp()
+# imu.read_mag()
+
+# print "Accelerometer: ", imu.accelerometer_data
+# print "Gyroscope:     ", imu.gyroscope_data
+# print "Temperature:   ", imu.temperature
+# print "Magnetometer:  ", imu.magnetometer_data
+# time.sleep(0.1)
+
+# If the IMU is upside down (Skull logo facing up), change this value to 1
+IMU_UPSIDE_DOWN = 0	
+
+
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070  	# [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+AA =  0.40      	# Complementary filter constant
+MAG_LPF_FACTOR = 0.4 	# Low pass filter constant magnetometer
+ACC_LPF_FACTOR = 0.4 	# Low pass filter constant for accelerometer
+ACC_MEDIANTABLESIZE = 9    	# Median filter table size for accelerometer. Higher = smoother but a longer delay
+MAG_MEDIANTABLESIZE = 9    	# Median filter table size for magnetometer. Higher = smoother but a longer delay
+
+
+
+################# Compass Calibration values ############
+# Use calibrateBerryIMU.py to get calibration values 
+# Calibrating the compass isnt mandatory, however a calibrated 
+# compass will result in a more accurate heading value.
+
+magXmin =  0
+magYmin =  0
+magZmin =  0
+magXmax =  0
+magYmax =  0
+magZmax =  0
+
+
+
+#Kalman filter variables
+Q_angle = 0.02
+Q_gyro = 0.0015
+R_angle = 0.005
+y_bias = 0.0
+x_bias = 0.0
+XP_00 = 0.0
+XP_01 = 0.0
+XP_10 = 0.0
+XP_11 = 0.0
+YP_00 = 0.0
+YP_01 = 0.0
+YP_10 = 0.0
+YP_11 = 0.0
+KFangleX = 0.0
+KFangleY = 0.0
+
+def kalmanFilterY ( accAngle, gyroRate, DT):
+	y=0.0
+	S=0.0
+
+	global KFangleY
+	global Q_angle
+	global Q_gyro
+	global y_bias
+	global YP_00
+	global YP_01
+	global YP_10
+	global YP_11
+
+	KFangleY = KFangleY + DT * (gyroRate - y_bias)
+
+	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
+	YP_01 = YP_01 + ( - DT * YP_11 )
+	YP_10 = YP_10 + ( - DT * YP_11 )
+	YP_11 = YP_11 + ( + Q_gyro * DT )
+
+	y = accAngle - KFangleY
+	S = YP_00 + R_angle
+	K_0 = YP_00 / S
+	K_1 = YP_10 / S
+	
+	KFangleY = KFangleY + ( K_0 * y )
+	y_bias = y_bias + ( K_1 * y )
+	
+	YP_00 = YP_00 - ( K_0 * YP_00 )
+	YP_01 = YP_01 - ( K_0 * YP_01 )
+	YP_10 = YP_10 - ( K_1 * YP_00 )
+	YP_11 = YP_11 - ( K_1 * YP_01 )
+	
+	return KFangleY
+
+def kalmanFilterX ( accAngle, gyroRate, DT):
+	x=0.0
+	S=0.0
+
+	global KFangleX
+	global Q_angle
+	global Q_gyro
+	global x_bias
+	global XP_00
+	global XP_01
+	global XP_10
+	global XP_11
+
+
+	KFangleX = KFangleX + DT * (gyroRate - x_bias)
+
+	XP_00 = XP_00 + ( - DT * (XP_10 + XP_01) + Q_angle * DT )
+	XP_01 = XP_01 + ( - DT * XP_11 )
+	XP_10 = XP_10 + ( - DT * XP_11 )
+	XP_11 = XP_11 + ( + Q_gyro * DT )
+
+	x = accAngle - KFangleX
+	S = XP_00 + R_angle
+	K_0 = XP_00 / S
+	K_1 = XP_10 / S
+	
+	KFangleX = KFangleX + ( K_0 * x )
+	x_bias = x_bias + ( K_1 * x )
+	
+	XP_00 = XP_00 - ( K_0 * XP_00 )
+	XP_01 = XP_01 - ( K_0 * XP_01 )
+	XP_10 = XP_10 - ( K_1 * XP_00 )
+	XP_11 = XP_11 - ( K_1 * XP_01 )
+	
+	return KFangleX
+
+
+gyroXangle = 0.0
+gyroYangle = 0.0
+gyroZangle = 0.0
+CFangleX = 0.0
+CFangleY = 0.0
+CFangleXFiltered = 0.0
+CFangleYFiltered = 0.0
+kalmanX = 0.0
+kalmanY = 0.0
+oldXMagRawValue = 0
+oldYMagRawValue = 0
+oldZMagRawValue = 0
+oldXAccRawValue = 0
+oldYAccRawValue = 0
+oldZAccRawValue = 0
+
+a = datetime.datetime.now()
+
+
+
+#Setup the tables for the mdeian filter. Fill them all with '1' soe we dont get devide by zero error 
+acc_medianTable1X = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable1Y = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable1Z = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable2X = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable2Y = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable2Z = [1] * ACC_MEDIANTABLESIZE
+mag_medianTable1X = [1] * MAG_MEDIANTABLESIZE
+mag_medianTable1Y = [1] * MAG_MEDIANTABLESIZE
+mag_medianTable1Z = [1] * MAG_MEDIANTABLESIZE
+mag_medianTable2X = [1] * MAG_MEDIANTABLESIZE
+mag_medianTable2Y = [1] * MAG_MEDIANTABLESIZE
+mag_medianTable2Z = [1] * MAG_MEDIANTABLESIZE
+
+
+def EulerAngles():
+
+
+    m9a, m9g, m9m = imu.getMotion9()
+    #Read the accelerometer,gyroscope and magnetometer values
+    ACCx = m9a[0]
+    ACCy = m9a[1]
+    ACCz = m9a[2]
+    GYRx = m9g[0]
+    GYRy = m9g[1]
+    GYRz = m9g[2]
+    MAGx = m9m[0]
+    MAGy = m9m[1]
+    MAGz = m9m[2]
+
+
+    #Apply compass calibration    
+    MAGx -= (magXmin + magXmax) /2 
+    MAGy -= (magYmin + magYmax) /2 
+    MAGz -= (magZmin + magZmax) /2 
+ 
+
+    ##Calculate loop Period(LP). How long between Gyro Reads
+    b = datetime.datetime.now() - a
+    a = datetime.datetime.now()
+    LP = b.microseconds/(1000000*1.0)
+
+    ############################################### 
+    #### Apply low pass filter ####
+    ###############################################
+    MAGx =  MAGx  * MAG_LPF_FACTOR + oldXMagRawValue*(1 - MAG_LPF_FACTOR);
+    MAGy =  MAGy  * MAG_LPF_FACTOR + oldYMagRawValue*(1 - MAG_LPF_FACTOR);
+    MAGz =  MAGz  * MAG_LPF_FACTOR + oldZMagRawValue*(1 - MAG_LPF_FACTOR);
+    ACCx =  ACCx  * ACC_LPF_FACTOR + oldXAccRawValue*(1 - ACC_LPF_FACTOR);
+    ACCy =  ACCy  * ACC_LPF_FACTOR + oldYAccRawValue*(1 - ACC_LPF_FACTOR);
+    ACCz =  ACCz  * ACC_LPF_FACTOR + oldZAccRawValue*(1 - ACC_LPF_FACTOR);
+
+    oldXMagRawValue = MAGx
+    oldYMagRawValue = MAGy
+    oldZMagRawValue = MAGz
+    oldXAccRawValue = ACCx
+    oldYAccRawValue = ACCy
+    oldZAccRawValue = ACCz
+
+    ######################################### 
+    #### Median filter for accelerometer ####
+    #########################################
+    # cycle the table
+    for x in range (ACC_MEDIANTABLESIZE-1,0,-1 ):
+        acc_medianTable1X[x] = acc_medianTable1X[x-1]
+        acc_medianTable1Y[x] = acc_medianTable1Y[x-1]
+        acc_medianTable1Z[x] = acc_medianTable1Z[x-1]
+
+    # Insert the lates values
+    acc_medianTable1X[0] = ACCx
+    acc_medianTable1Y[0] = ACCy
+    acc_medianTable1Z[0] = ACCz    
+
+    # Copy the tables
+    acc_medianTable2X = acc_medianTable1X[:]
+    acc_medianTable2Y = acc_medianTable1Y[:]
+    acc_medianTable2Z = acc_medianTable1Z[:]
+
+    # Sort table 2
+    acc_medianTable2X.sort()
+    acc_medianTable2Y.sort()
+    acc_medianTable2Z.sort()
+
+    # The middle value is the value we are interested in
+    ACCx = acc_medianTable2X[ACC_MEDIANTABLESIZE/2];
+    ACCy = acc_medianTable2Y[ACC_MEDIANTABLESIZE/2];
+    ACCz = acc_medianTable2Z[ACC_MEDIANTABLESIZE/2];
+
+
+
+    ######################################### 
+    #### Median filter for magnetometer ####
+    #########################################
+    # cycle the table
+    for x in range (MAG_MEDIANTABLESIZE-1,0,-1 ):
+        mag_medianTable1X[x] = mag_medianTable1X[x-1]
+        mag_medianTable1Y[x] = mag_medianTable1Y[x-1]
+        mag_medianTable1Z[x] = mag_medianTable1Z[x-1]
+
+    # Insert the latest values    
+    mag_medianTable1X[0] = MAGx
+    mag_medianTable1Y[0] = MAGy
+    mag_medianTable1Z[0] = MAGz    
+
+    # Copy the tables
+    mag_medianTable2X = mag_medianTable1X[:]
+    mag_medianTable2Y = mag_medianTable1Y[:]
+    mag_medianTable2Z = mag_medianTable1Z[:]
+
+    # Sort table 2
+    mag_medianTable2X.sort()
+    mag_medianTable2Y.sort()
+    mag_medianTable2Z.sort()
+
+    # The middle value is the value we are interested in
+    MAGx = mag_medianTable2X[MAG_MEDIANTABLESIZE/2];
+    MAGy = mag_medianTable2Y[MAG_MEDIANTABLESIZE/2];
+    MAGz = mag_medianTable2Z[MAG_MEDIANTABLESIZE/2];
+
+
+
+    #Convert Gyro raw to degrees per second
+    rate_gyr_x =  GYRx * G_GAIN
+    rate_gyr_y =  GYRy * G_GAIN
+    rate_gyr_z =  GYRz * G_GAIN
+
+
+    #Calculate the angles from the gyro. 
+    gyroXangle+=rate_gyr_x*LP
+    gyroYangle+=rate_gyr_y*LP
+    gyroZangle+=rate_gyr_z*LP
+
+    #Convert Accelerometer values to degrees
+
+    if not IMU_UPSIDE_DOWN:
+        # If the IMU is up the correct way (Skull logo facing down), use these calculations
+        AccXangle =  (math.atan2(ACCy,ACCz)*RAD_TO_DEG)
+        AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
+    else:
+        #Us these four lines when the IMU is upside down. Skull logo is facing up
+        AccXangle =  (math.atan2(-ACCy,-ACCz)*RAD_TO_DEG)
+        AccYangle =  (math.atan2(-ACCz,-ACCx)+M_PI)*RAD_TO_DEG
+
+
+
+    #Change the rotation value of the accelerometer to -/+ 180 and
+    #move the Y axis '0' point to up.  This makes it easier to read.
+    if AccYangle > 90:
+        AccYangle -= 270.0
+    else:
+        AccYangle += 90.0
+
+
+
+    #Complementary filter used to combine the accelerometer and gyro values.
+    CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
+    CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
+
+    #Kalman filter used to combine the accelerometer and gyro values.
+    kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
+    kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
+
+    if IMU_UPSIDE_DOWN:
+        MAGy = -MAGy      #If IMU is upside down, this is needed to get correct heading.
+    #Calculate heading
+    heading = 180 * math.atan2(MAGy,MAGx)/M_PI
+
+    #Only have our heading between 0 and 360
+    if heading < 0:
+        heading += 360
+
+
+
+    ####################################################################
+    ###################Tilt compensated heading#########################
+    ####################################################################
+    #Normalize accelerometer raw values.
+    if not IMU_UPSIDE_DOWN:        
+        #Use these two lines when the IMU is up the right way. Skull logo is facing down
+        accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+        accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+    else:
+        #Us these four lines when the IMU is upside down. Skull logo is facing up
+        accXnorm = -ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+        accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+
+    #Calculate pitch and roll
+
+    pitch = math.asin(accXnorm)
+    roll = -math.asin(accYnorm/math.cos(pitch))
+
+
+    #Calculate the new tilt compensated values
+    magXcomp = MAGx*math.cos(pitch)+MAGz*math.sin(pitch)
+ 
+    #The compass and accelerometer are orientated differently on the LSM9DS0 and LSM9DS1 and the Z axis on the compass
+    #is also reversed. This needs to be taken into consideration when performing the calculations
+    #if(IMU.LSM9DS0):
+    #    magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)-MAGz*math.sin(roll)*math.cos(pitch)   #LSM9DS0
+    #else:
+    #    magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)+MAGz*math.sin(roll)*math.cos(pitch)   #LSM9DS1
+
+
+
+
+	#Calculate tilt compensated heading
+    tiltCompensatedHeading = 180 * math.atan2(magYcomp,magXcomp)/M_PI
+
+    if tiltCompensatedHeading < 0:
+                tiltCompensatedHeading += 360
+
+    ############################ END ##################################
+
+    return kalmanX, kalmanY, tiltCompensatedHeading
+
+    #slow program down a bit, makes the output more readable
+    time.sleep(0.03)
+
+
+baro = ms5611.MS5611()
+baro.initialize()
+
+def HypsometricFormula(pres, temp):
+	PressureInitOverCurrentPres = 1012.5/pres
+	TempinKelvin = 273.15+temp
+	return ((PressureInitOverCurrentPres**(1/5.257)-1)*TempinKelvin)/.0065
+
+def MetersToFeet(meters):
+	return meters*3.28084
+
+def deltaAltitude():
+	return MetersToFeet(HypsometricFormula(baro.PRES, baro.TEMP)) - Offset
+
+def BarometerReading():
+	baro.refreshPressure()
+	time.sleep(0.01) # Waiting for pressure data ready 10ms
+	baro.readPressure()
+
+	baro.refreshTemperature()
+	time.sleep(0.01) # Waiting for temperature data ready 10ms
+	baro.readTemperature()
+
+	baro.calculatePressureAndTemperature()
+	return MetersToFeet(HypsometricFormula(baro.PRES, baro.TEMP))
+
+
+
+###################################################################
+###################################################################
 class PID:
-    """PID Controller
-    """
+	"""
+	Discrete PID control
+	"""
 
-    def __init__(self, P=0.2, I=0.0, D=0.0):
+	def __init__(self, P=1.0, I=0.1, D=0.01, Derivator=0, Integrator=0, Integrator_max=500, Integrator_min=-500):
 
-        self.Kp = P
-        self.Ki = I
-        self.Kd = D
+		self.Kp=P
+		self.Ki=I
+		self.Kd=D
+		self.Derivator=Derivator
+		self.Integrator=Integrator
+		self.Integrator_max=Integrator_max
+		self.Integrator_min=Integrator_min
 
-        self.sample_time = 0.00
-        self.current_time = time.time()
-        self.last_time = self.current_time
+		self.set_point=0.0
+		self.error=0.0
 
-        self.clear()
+	def update(self,current_value):
+		"""
+		Calculate PID output value for given reference input and feedback
+		"""
 
-    def clear(self):
-        """Clears PID computations and coefficients"""
-        self.SetPoint = 0.0
+		self.error = self.set_point - current_value
 
-        self.PTerm = 0.0
-        self.ITerm = 0.0
-        self.DTerm = 0.0
-        self.last_error = 0.0
+		self.P_value = self.Kp * self.error
+		self.D_value = self.Kd * ( self.error - self.Derivator)
+		self.Derivator = self.error
 
-        # Windup Guard
-        self.int_error = 0.0
-        self.windup_guard = 20.0
+		self.Integrator = self.Integrator + self.error
 
-        self.output = 0.0
+		if self.Integrator > self.Integrator_max:
+			self.Integrator = self.Integrator_max
+		elif self.Integrator < self.Integrator_min:
+			self.Integrator = self.Integrator_min
 
-    def update(self, feedback_value):
-        """Calculates PID value for given reference feedback
-        .. math::
-            u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
-        .. figure:: images/pid_1.png
-           :align:   center
-           Test PID with Kp=1.2, Ki=1, Kd=0.001 (test_pid.py)
-        """
-        error = self.SetPoint - feedback_value
+		self.I_value = self.Integrator * self.Ki
 
-        self.current_time = time.time()
-        delta_time = self.current_time - self.last_time
-        delta_error = error - self.last_error
+		PID = self.P_value + self.I_value + self.D_value
 
-        if (delta_time >= self.sample_time):
-            self.PTerm = self.Kp * error
-            self.ITerm += error * delta_time
+		return PID
 
-            if (self.ITerm < -self.windup_guard):
-                self.ITerm = -self.windup_guard
-            elif (self.ITerm > self.windup_guard):
-                self.ITerm = self.windup_guard
+	def setPoint(self,set_point):
+		"""
+		Initilize the setpoint of PID
+		"""
+		self.set_point = set_point
+		self.Integrator=0
+		self.Derivator=0
 
-            self.DTerm = 0.0
-            if delta_time > 0:
-                self.DTerm = delta_error / delta_time
+	def setIntegrator(self, Integrator):
+		self.Integrator = Integrator
 
-            # Remember last time and last error for next calculation
-            self.last_time = self.current_time
-            self.last_error = error
+	def setDerivator(self, Derivator):
+		self.Derivator = Derivator
 
-            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+	def setKp(self,P):
+		self.Kp=P
 
-    def setKp(self, proportional_gain):
-        """Determines how aggressively the PID reacts to the current error with setting Proportional Gain"""
-        self.Kp = proportional_gain
+	def setKi(self,I):
+		self.Ki=I
 
-    def setKi(self, integral_gain):
-        """Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
-        self.Ki = integral_gain
+	def setKd(self,D):
+		self.Kd=D
 
-    def setKd(self, derivative_gain):
-        """Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
-        self.Kd = derivative_gain
+	def getPoint(self):
+		return self.set_point
 
-    def setWindup(self, windup):
-        """Integral windup, also known as integrator windup or reset windup,
-        refers to the situation in a PID feedback controller where
-        a large change in setpoint occurs (say a positive change)
-        and the integral terms accumulates a significant error
-        during the rise (windup), thus overshooting and continuing
-        to increase as this accumulated error is unwound
-        (offset by errors in the other direction).
-        The specific problem is the excess overshooting.
-        """
-        self.windup_guard = windup
+	def getError(self):
+		return self.error
 
-    def setSampleTime(self, sample_time):
-        """PID that should be updated at a regular interval.
-        Based on a pre-determined sampe time, the PID decides if it should compute or return immediately.
-        """
-        self.sample_time = sample_time
+	def getIntegrator(self):
+		return self.Integrator
+
+	def getDerivator(self):
+		return self.Derivator
+
+######	Example	#########
+#
+p=PID(1.0,0.01,0.01)
+p.setPoint(1.0)
+while True:
+    x, y, z = Eulerangles()
+    pid = p.update(x)
+    print(pid)
+    time.sleep(0.03)
