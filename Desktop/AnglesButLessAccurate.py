ba914b57cf02e8c8adb90fb8ed86c1f48a2ba7da
sewald@commschool.org <sewald@commschool.org>
2019-05-16 19:27:40 -0400

    es

--- Desktop/AnglesButLessAccurate.py
+++ Desktop/AnglesButLessAccurate.py
@@ -392,7 +392,7 @@ while True:
         return angle
 
 
-    #def mag2tiltcomp(bx, by, bz, phi, theta):
+    def mag2tiltcomp(bx, by, bz, phi, theta):
         #""" Takes in raw magnetometer values, pitch and roll and turns it into a tilt-compensated heading value ranging from -pi to pi (everything in this function should be in radians). """
         #variation = 4.528986*(pi/180) # magnetic variation for Corpus Christi, should match your bx/by/bz and where they were measured from (a lookup table is beyond the scope of this gist)
         variation = -0.254236 #for boston
