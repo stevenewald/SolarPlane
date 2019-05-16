6461a7ca6b79a36d5c8d39d78be4b6adaa2758c8
stevenewald <47366039+stevenewald@users.noreply.github.com>
2019-05-16 13:54:02 -0400

    Create calibration.py

---
+++ Desktop/calibration.py
@@ -0,0 +1,50 @@
+import time
+import sys
+import navio.ms5611
+import navio.util
+import argparse 
+import navio.mpu9250
+import navio.rcinput
+import math
+import navio.pwm
+import datetime
+import os
+
+navio.util.check_apm()
+
+imu = navio.mpu9250.MPU9250()
+
+imu.initialize()
+
+
+while True:
+
+    #Read magnetometer values
+    m9a, m9g, m9m = imu.getMotion9()
+
+    MAGx = m9m[0]
+    MAGy = m9m[1]
+    MAGz = m9m[2]
+    
+    
+    
+    if MAGx > magXmax:
+        magXmax = MAGx
+    if MAGy > magYmax:
+        magYmax = MAGy
+    if MAGz > magZmax:
+        magZmax = MAGz
+
+    if MAGx < magXmin:
+        magXmin = MAGx
+    if MAGy < magYmin:
+        magYmin = MAGy
+    if MAGz < magZmin:
+        magZmin = MAGz
+
+    print(" magXmin  %i  magYmin  %i  magZmin  %i  ## magXmax  %i  magYmax  %i  magZmax %i  " %(magXmin,magYmin,magZmin,magXmax,magYmax,magZmax))
+
+
+
+    #slow program down a bit, makes the output more readable
+    time.sleep(0.03)
