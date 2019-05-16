5ef2ccde54913f03b146afca8f42555f8c31b2ed
stevenewald <47366039+stevenewald@users.noreply.github.com>
2019-05-16 13:55:40 -0400

    Update calibration.py

--- Desktop/calibration.py
+++ Desktop/calibration.py
@@ -10,11 +10,35 @@ import navio.pwm
 import datetime
 import os
 
+def handle_ctrl_c(signal, frame):
+    print " "
+    print "magXmin = ",  magXmin
+    print "magYmin = ",  magYmin
+    print "magZmin = ",  magZmin
+    print "magXmax = ",  magXmax
+    print "magYmax = ",  magYmax
+    print "magZmax = ",  magZmax
+    sys.exit(130) # 130 is standard exit code for ctrl-c
+
+#This will capture exit when using Ctrl-C
+signal.signal(signal.SIGINT, handle_ctrl_c)
+
+
+a = datetime.datetime.now()
+
 navio.util.check_apm()
 
 imu = navio.mpu9250.MPU9250()
 
 imu.initialize()
+
+#Preload the variables used to keep track of the minimum and maximum values
+magXmin = 32767
+magYmin = 32767
+magZmin = 32767
+magXmax = -32767
+magYmax = -32767
+magZmax = -32767
 
 
 while True:
