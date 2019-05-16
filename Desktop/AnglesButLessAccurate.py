d7f9292c70511d22349929da713acaa3558988f0
sewald@commschool.org <sewald@commschool.org>
2019-05-16 19:27:10 -0400

    es

--- Desktop/AnglesButLessAccurate.py
+++ Desktop/AnglesButLessAccurate.py
@@ -382,25 +382,26 @@ while True:
 
 	#Calculate tilt compensated heading
     tiltCompensatedHeading = 180 * math.atan2(magYcomp,magXcomp)/M_PI
-    #def wrap(angle):
-    #    if angle > M_PI:
-    #        angle -= (2*M_PI)
-    #    if angle < -M_PI:
-    #        angle += (2*M_PI)
-    #    if angle < 0:
-    #        angle += 2*M_PI
-    #    return angle
+    def wrap(angle):
+        if angle > M_PI:
+            angle -= (2*M_PI)
+        if angle < -M_PI:
+            angle += (2*M_PI)
+        if angle < 0:
+            angle += 2*M_PI
+        return angle
 
 
     #def mag2tiltcomp(bx, by, bz, phi, theta):
-    #    #""" Takes in raw magnetometer values, pitch and roll and turns it into a tilt-compensated heading value ranging from -pi to pi (everything in this function should be in radians). """
-    #    #variation = 4.528986*(pi/180) # magnetic variation for Corpus Christi, should match your bx/by/bz and where they were measured from (a lookup table is beyond the scope of this gist)
-    #    variation = -0.254236 #for boston
-    #    Xh = bx * math.cos(theta) + by * math.sin(phi) * math.sin(theta) + bz * math.cos(phi) * math.sin(theta)
-    #    Yh = by * math.cos(phi) - bz * math.sin(phi)
-    #    return wrap((math.atan2(-Yh, Xh) + variation))
+        #""" Takes in raw magnetometer values, pitch and roll and turns it into a tilt-compensated heading value ranging from -pi to pi (everything in this function should be in radians). """
+        #variation = 4.528986*(pi/180) # magnetic variation for Corpus Christi, should match your bx/by/bz and where they were measured from (a lookup table is beyond the scope of this gist)
+        variation = -0.254236 #for boston
+        Xh = bx * math.cos(theta) + by * math.sin(phi) * math.sin(theta) + bz * math.cos(phi) * math.sin(theta)
+        Yh = by * math.cos(phi) - bz * math.sin(phi)
+        return wrap((math.atan2(-Yh, Xh) + variation))
     
-    #tiltCompensatedHeading = mag2tiltcomp(m9m[0], m9m[1], m9m[2], pitch, roll)
+    tiltCompensatedHeading = mag2tiltcomp(m9m[0], m9m[1], m9m[2], pitch, roll)
+    tiltCompensatedHeading = tiltCompensatedHeading*57.29578 #convert from radians to degrees
 
     if tiltCompensatedHeading < 0:
                 tiltCompensatedHeading += 360
