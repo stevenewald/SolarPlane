36e1ac9420df90af7afdbfd103516c832e15cf83
sewald@commschool.org <sewald@commschool.org>
2019-05-22 19:48:17 -0400

    whywontthiswork

--- C++/Examples/AHRS/AHRS.cpp
+++ C++/Examples/AHRS/AHRS.cpp
@@ -471,9 +471,9 @@ void imuLoop(AHRS* ahrs, Socket sock)
     {
         // Console output
         //printf("ROLL: %+05.2f PITCH: %+05.2f YAW: %+05.2f PERIOD %.4fs RATE %dHz \n", roll, pitch, yaw * -1, dt, int(1/dt));
-        cout << roll;
-        cout << pitch;
-        cout << (yaw * -1);
+        cout << roll << endl;
+        cout << pitch << endl;
+        cout << (yaw * -1) << endl;
 
         // Network output
         //sock.output( ahrs->getW(), ahrs->getX(), ahrs->getY(), ahrs->getZ(), int(1/dt));//#
