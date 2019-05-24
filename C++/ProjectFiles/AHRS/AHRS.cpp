cbd0ee21b7f726e1c8f6fcb29b46facd4cf4effa
sewald@commschool.org <sewald@commschool.org>
2019-05-24 18:58:40 -0400

    altitude addition

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -422,7 +422,7 @@ void imuLoop(AHRS* ahrs)
     barometer.readTemperature();
 
     barometer.calculatePressureAndTemperature();
-    print(barometer.getPressure())
+    printf(barometer.getPressure())
 
     altitudeInFeet = (ahrs->HypFormula(barometer.getPressure(), barometer.getTemperature()))*3.28084;
     //--------read raw measurements from the MPU and update AHRS--------------
