cbbca91068c40ae33108cad7359d4fa57984f50d
sewald@commschool.org <sewald@commschool.org>
2019-06-05 19:19:37 -0400

    added servo stuff

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -405,9 +405,9 @@ void imuLoop(AHRS* ahrs)
     if(firstTimeRunningRcinput){
         rcin->initialize();
         //pwm->initialize(1);//throttle
-        //pwm->initialize(2);//elevator
-        //pwm->initialize(3);//rudder
-        //pwm->initialize(4);//spoiler
+        pwm->initialize(2);//elevator
+        pwm->initialize(3);//rudder
+        pwm->initialize(4);//spoiler
         //pwm->set_frequency(1, 50);
         pwm->set_frequency(2, 50);
         pwm->set_frequency(3, 50);
