83f3c6cf852279bbd339fd952ec8590cec7202bc
sewald@commschool.org <sewald@commschool.org>
2019-06-05 19:09:45 -0400

    added servo stuff

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -482,10 +482,6 @@ void imuLoop(AHRS* ahrs)
     printf(inputElev);
     //manualoverride = rcin->read(3)
 
-    if ( !(pwm->enable(PWM_OUTPUT)) ) {
-	    return 1;
-	}
-
     //apply input to servos
     pwm->set_duty_cycle(2, inputElev);
     pwm->set_duty_cycle(3, inputRudd);
