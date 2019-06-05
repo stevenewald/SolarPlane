77905212648ebcbb6b9e8009586b5021592932f0
sewald@commschool.org <sewald@commschool.org>
2019-06-05 19:35:38 -0400

    added servo stuff

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -405,11 +405,11 @@ void imuLoop(AHRS* ahrs)
     if(firstTimeRunningRcinput){
         rcin->initialize();
         //pwm->initialize(1);//throttle
-        //pwm->initialize(2);//elevator
+        pwm->initialize(2);//elevator
         pwm->initialize(3);//rudder
         pwm->initialize(4);//spoiler
         //pwm->set_frequency(1, 50);
-        //pwm->set_frequency(2, 50);
+        pwm->set_frequency(2, 50);
         pwm->set_frequency(3, 50);
         pwm->set_frequency(4, 50);
         
@@ -483,7 +483,7 @@ void imuLoop(AHRS* ahrs)
     //manualoverride = rcin->read(3)
 
     //apply input to servos
-    //pwm->set_duty_cycle(2, inputElev);
+    pwm->set_duty_cycle(2, inputElev);
     pwm->set_duty_cycle(3, inputRudd);
     //pwm->set_duty_cycle(4, inputSpoilers);
 
