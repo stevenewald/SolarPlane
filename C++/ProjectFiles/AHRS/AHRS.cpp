a55e232af126e00d37db950b4a7c69b1b21ec103
sewald@commschool.org <sewald@commschool.org>
2019-07-10 20:11:20 -0400

    thrott

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -571,7 +571,7 @@ void imuLoop(AHRS* ahrs, int* phaseOfFlightVal, int* firstTimeRunningRcinput)
     }
  
     float elevatorComp;
-    elevatorComp = (1.5+(pow(roll, 1.2))/221);
+    elevatorComp = (1.5+(pow(abs(roll-180), 1.2))/221);
     if(*phaseOfFlightVal==3) 
     {
         //pwm->set_duty_cycle(2, elevatorComp);
