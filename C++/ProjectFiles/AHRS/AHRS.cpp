566797cde2c3a37c1e59c9b0144d9c2efa527301
sewald@commschool.org <sewald@commschool.org>
2019-09-04 19:11:52 -0400

    updates

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -634,8 +634,7 @@ void imuLoop(AHRS* ahrs, int* phaseOfFlightVal, int* firstTimeRunningRcinput, in
     dtsumm += dt;
     //if(dtsumm > 0.05)
     *printcounter = *printcounter + 1;
-    //if(remainder(*printcounter, 200) == 0)
-    if(false)
+    if(remainder(*printcounter, 200) == 0)
     {
         // Console output
 
