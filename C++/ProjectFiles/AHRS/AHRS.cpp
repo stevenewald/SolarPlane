3c8146280b10d8112548408a31bee3fe5d7dc8b9
sewald@commschool.org <sewald@commschool.org>
2019-09-14 18:21:52 -0400

    changed imu

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -358,29 +358,6 @@ std::unique_ptr <InertialSensor> get_inertial_sensor( std::string sensor_name) /
 }
 
 
-std::string get_sensor_name(int argc, char *argv[])
-{
-    if (get_navio_version() == NAVIO2) {
-
-        if (argc < 2) {
-            printf("Enter parameter\n");
-            return std::string();
-        }
-
-        //prevent the error message
-        opterr = 0;
-        int parameter;
-
-        while ((parameter = getopt(argc, argv, "i:h")) != -1) {
-            switch (parameter) {
-            case 'i': return optarg;
-            case '?': printf("Wrong parameter.\n");
-                      return std::string();
-            }
-        }
-
-    }
-}
 
 /*
 //takes in current pitch and target pitch and returns corrected elevator servo
@@ -776,14 +753,11 @@ int main(int argc, char *argv[])
     if (check_apm()) {
         return 1;
     }
-    
-
-    auto sensor_name = get_sensor_name(argc, argv);
 
     if (sensor_name.empty())
         return EXIT_FAILURE;
 
-    auto imu = get_inertial_sensor(sensor_name);
+    auto imu = get_inertial_sensor("mpu");
 
     if (!imu) {
         printf("Wrong sensor name. Select: mpu or lsm\n"); //can use both IMUs
