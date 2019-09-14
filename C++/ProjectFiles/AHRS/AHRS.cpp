5a12c6bb8b1578d7b512e504372b7ca692160869
sewald@commschool.org <sewald@commschool.org>
2019-09-13 20:02:52 -0400

    outfile

--- C++/ProjectFiles/AHRS/AHRS.cpp
+++ C++/ProjectFiles/AHRS/AHRS.cpp
@@ -665,11 +665,15 @@ void imuLoop(AHRS* ahrs, int* phaseOfFlightVal, int* firstTimeRunningRcinput, in
     }
  
     float elevatorComp;
+    float minVal;
+    float maxVal;
+    minVal = -0.5;
+    maxVal = 0.5;
     //elevatorComp = (pow(abs(roll), 1.2));
     pid_set_gains(&pid, 2., 0.01, 0.001);
     elevatorComp = pid_process(&pid, (-1*roll+*gyroCalibElev));
-    elevatorComp = std::max(elevatorComp, -0.5);
-    elevatorComp = std::min(elevatorComp, 0.5);
+    elevatorComp = std::max(elevatorComp, minVal);
+    elevatorComp = std::min(elevatorComp, maxVal);
     if(roll > 0)  //this is for the non-pid controller, is redundant with it
     {
         elevatorComp = ((1.5+(elevatorComp)/100)*1000); //formatting for servo duty cycle
